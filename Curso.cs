d39094369acec0776f3a086781544121fd2933e9
Saulo Godoy Proetti <38708863+saulogp@users.noreply.github.com>
2022-10-21 02:26:18 -0300

    classe curso para futuras implementações (#1)

---
+++ Curso.cs
@@ -0,0 +1,10 @@
+namespace Revisao
+{
+    public class Curso
+    {
+        private string nome {get; set;}
+        private int carga_horaria {get; set;}
+        private int max_alunos {get; set;}
+        private string curriculo {get; set;}
+    }
+}
